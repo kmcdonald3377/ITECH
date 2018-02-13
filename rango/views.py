392fef2b08c0c1353db77b8409e14ca254b010a7
kmcdonald3377 <30431372+kmcdonald3377@users.noreply.github.com>
2018-02-13 17:28:48 +0000

    Chapter 10 beginning

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -17,7 +18,7 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
-
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories' : category_list, 'pages' : page_list}
@@ -28,6 +29,10 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    
     print(request.method)
     print(request.user)
     # Construct a dictionary to pass to the template engine as its context.
@@ -169,3 +174,15 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    
+    response.set_cookie('visits', visits)
